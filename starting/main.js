169bc89997fbb473ead919592236eb68d7245fa4
Aaron Long <aaronlng05@gmail.com>
2020-06-08 20:33:08 -0400

    step 2 and 3 in the find your had project. Need to work out bugs

--- starting/main.js
+++ starting/main.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
@@ -6,5 +6,14 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    
+    class Field {
+    const myField = new Field([
+        ['*', '░', 'O'],
+        ['░', 'O', '░'],
+        ['░', '^', '░'],
+    ]),
+    print() {
+
+    }
+};
 }
